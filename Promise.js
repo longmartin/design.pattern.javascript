542413834b77780216feaaa75883bb8af06a1242
longweiwei <long152martin@163.com>
2017-11-02 23:45:15 +0800

    Promise
    Promise模式

--- Promise.js
+++ Promise.js
@@ -1,22 +1,35 @@
 
 function Promise(fn) {
+	this.callback = []
+	
     this.all = function() {
 		
 	}
 	this.race = function() {
 		
 	}
-	this.resolve = function() {
-		
+	this.complete = function(type, result) {
+		while(this.callback[0]) {
+			this.callback.shift()[type](result)
+		}
 	}
-	this.reject = function() {
-		
+	this.resolve = function(result) {
+		this.complete('resolve', result)
+	}
+	this.reject = function(result) {
+		this.complete('reject', result)
 	}
     return this
 }
-Promise.prototype.then = function(resolve) {
-	
+Promise.prototype.then = function(successHandler) {
+	this.callbacks.push({
+        resolve: successHandler
+    });
+    return this;
 }
-Promise.prototype.catch = function(reject) {
-
+Promise.prototype.catch = function(failHandler) {
+	this.callbacks.push({
+        reject: failedHandler
+    });
+    return this;
 }
